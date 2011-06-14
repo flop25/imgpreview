692a8c8ca930389b77b95350d44874a927be2600
ddtddt <ddtddt@piwigo.org>
2011-06-14 06:31:20 +0000

    [extensions] - imgpreview - add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@11350 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['imgp_max_width'] = 'attēla maksimālais platums';
+$lang['imgp_max_height'] = 'attēla maksimālais augstums';
+$lang['imgp_send'] = 'Iesniegt !';
+
+$lang['imgp_conf_updated'] = 'Konfigurācija ir atjaunināta';
+
+$lang['imgp_howitworks'] = 'lietot :
+* imgPreview jQuery spraudnis<br>
+* Copyright (c) 2009 James Padolsey<br>
+* j@qd9.co.uk | http://james.padolsey.com<br>
+*  MIT un GPL dubultlicence.<br>
+* Atjaunināts: 09/02/09<br>
+* @autors James Padolsey<br>
+* @versija 0.22<br>
+';
+
+?>
