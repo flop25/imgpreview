90bb43f3b0c49d5b71032680b2aab704c7d986e2
ddtddt <ddtddt@piwigo.org>
2011-06-28 22:29:33 +0000

    [extensions] - imgpreview - add NL (Dutch - Nederlands) thanks to kees
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@11560 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,23 @@
+<?php
+$lang['imgp_max_width'] = 'maximale breedte van de afbeelding';
+$lang['imgp_max_height'] = 'maximale hoogte van de afbeelding';
+$lang['imgp_send'] = 'Verstuur !';
+
+$lang['imgp_conf_updated'] = 'De configuratie is bijgewerkt';
+
+
+
+
+$lang['imgp_howitworks'] = 'use : 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>
+';
+
+
+
+?>
