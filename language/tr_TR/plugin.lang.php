35fe2ad3470f39fbbaad619df240edaf43ccbe3a
Nakre <Nakre@piwigo.org>
2011-06-28 17:07:59 +0000

    added tr_TR Turkish language
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@11554 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+$lang['imgp_max_width'] = 'resmin maksimum genişliği';
+$lang['imgp_max_height'] = 'resmin maksimum yüksekliği';
+$lang['imgp_send'] = 'Gönder !';
+
+$lang['imgp_conf_updated'] = 'Yapılandırma güncellendi';
+
+
+
+$lang['imgp_howitworks'] = 'kullan : 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>
+';
+
+
+
+?>
