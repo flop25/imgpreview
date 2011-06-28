851328eb9ea9a6b46bd9e3bd0df51657a33c902f
ddtddt <ddtddt@piwigo.org>
2011-06-28 16:50:02 +0000

    [extensions] - imgpreview - add PL (Polish - Polski) thanks to kuba
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@11549 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+$lang['imgp_max_width'] = 'maksymalna szerokość obrazu';
+$lang['imgp_max_height'] = 'maksymalna wysokość obrazu';
+$lang['imgp_send'] = 'Wyślij !';
+
+$lang['imgp_conf_updated'] = 'Konfiguracja została zaktualizowana';
+
+
+
+$lang['imgp_howitworks'] = 'use : 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>
+';
+
+
+
+?>
