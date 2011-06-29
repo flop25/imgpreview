09d9a29bed8f128a87890c058fd8a76f6434d074
ddtddt <ddtddt@piwigo.org>
2011-06-29 06:03:06 +0000

    [extensions] - imgpreview - add RU (Русский - Russian) thanks to yuragal
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@11561 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,21 @@
+<?php
+$lang['imgp_max_width'] = 'максимальная ширина фотографии';
+$lang['imgp_max_height'] = 'максимальная высота фотографии';
+$lang['imgp_send'] = 'Сохранить';
+
+$lang['imgp_conf_updated'] = 'Настройки сохранены';
+
+
+
+$lang['imgp_howitworks'] = 'О плагине :
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>
+';
+
+
+?>
