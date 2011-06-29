2816538947eee5bbd677f288a8925cb95fc9691a
ddtddt <ddtddt@piwigo.org>
2011-06-29 20:44:49 +0000

    [extensions] - imgpreview - add CZ (Česky - Czech) thanks to puschpull
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@11581 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,19 @@
+<?php
+
+$lang['imgp_max_width'] = 'maximální šířka fotografie';
+$lang['imgp_max_height'] = 'maximální výška fotografie';
+$lang['imgp_send'] = 'odeslat !';
+$lang['imgp_conf_updated'] = 'konfigurace byla aktualizována';
+
+
+$lang['imgp_howitworks'] = 'use : 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>
+';
+
+?>
