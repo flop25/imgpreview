b57c97bcedeb61eb444460d6766fb555a5bdc391
samli <samli@piwigo.org>
2011-07-04 18:32:07 +0000

    [extensions] [imgpreview] add HU language
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@11634 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+$lang['imgp_max_width'] = 'kép max. szélessége';
+$lang['imgp_max_height'] = 'kép max. magassága';
+$lang['imgp_send'] = 'Küldés !';
+
+$lang['imgp_conf_updated'] = 'A konfiguráció frissítése sikeres.';
+
+
+
+$lang['imgp_howitworks'] = 'használat :<br>
+ * imgPreview jQuery bővítmény<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Kettős licenc alatt: MIT és GPL.<br>
+ * Frissítve: 09/02/09<br>
+ * @szerző James Padolsey<br>
+ * @verzió 0.22<br>
+';
+
+
+
+?>
