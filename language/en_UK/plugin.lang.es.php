0bd81145a2d96b7ffa3ad100b956cbd5de17da7d
am <am@piwigo.org>
2011-07-11 21:18:38 +0000

    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@11713 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/en_UK/plugin.lang.es.php
@@ -0,0 +1,22 @@
+<?php
+$lang['imgp_max_width'] = 'maximum width of the picture';
+$lang['imgp_max_height'] = 'maximum height of the picture';
+$lang['imgp_send'] = 'Submit !';
+
+$lang['imgp_conf_updated'] = 'The config has been updated';
+
+
+
+$lang['imgp_howitworks'] = 'use : 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>
+';
+
+
+
+?>
