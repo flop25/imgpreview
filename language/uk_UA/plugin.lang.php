057dd88bbe9dc9b913883f55e3eecc9d454f8443
flop25 <flop25@piwigo.org>
2012-02-17 09:01:47 +0000

    adding uk_UA thx to animan (pixobox.org)
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@13205 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+$lang['imgp_max_width'] = 'максимальна ширина світлини';
+$lang['imgp_max_height'] = 'максимальна висота світлини';
+$lang['imgp_send'] = 'Зберегти';
+
+$lang['imgp_conf_updated'] = 'Налаштування збережені';
+$lang['imgp_showtitle'] = 'Відображення назви світлини';/** TODO **/
+$lang['imgp_opacity'] = 'Прозорість при наведенні курсору миші';/** TODO **/
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> - автор цього плаґіну, рекомендує :
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> автор P@t : цей плаґін додає модулі у вашу галерею.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> автор Flop25 : цей плаґін дозволяє вам використовувати інші значки на будь-які теми</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> автор Flop25 : flop_style це "шаблон розширення" допомогає змінити невелику частину Piwigo, такі як відображення категорій</li>
+<li><a href="http://piwigo.org/ext/">і багато іншого на piwigo.org</a></li>
+</ul>
+<br>
+Для дизайнерів: виринаючі налаштовується з id #tooltip (з IMG або діапазон для тексту)
+<br>
+Автор української локалізації - animan(<a href="http://pixobox.org/">pixobox.org</a>)
+<br>
+<br>
+Цей плаґін використовує модифіковану версію:<br>
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>
+';/** TODO **/
+
+
+?>
