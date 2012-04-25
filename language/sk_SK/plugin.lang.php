c1906306a0d73e0430ede6357ced0b82a8d7bddc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-25 11:51:49 +0000

    [image_preview] Add sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@14411 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_conf_updated'] = 'Konfigurácia bola aktualizovaná';
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> - tvorca doplnku - odporúčania:
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t : tento doplnok pridáva moduly do Vašej galérie.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25 : tento doplnok povoľuje použitie iných ikon pre ktorúkoľvek tému</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> by Flop25 : flop_style je "rozšírená šablóna" a povoľuje zmenu malých častí Piwigo ako je zobrazenie kategórií</li>
+<li><a href="http://piwigo.org/ext/">a oveľa viac na piwigo.org</a></li>
+</ul>
+<br>
+Pre dizajnérov: vyskakovacie okno je upraviteľné s id #tooltip (s fotkou alebo veľkosťou pre text)
+<br>
+<br>
+Doplnok používa modifikované verzie:<br>
+
+* imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>
+';
+$lang['imgp_max_height'] = 'maximálna výška fotky';
+$lang['imgp_max_width'] = 'maximálna šírka fotky';
+$lang['imgp_opacity'] = 'Neprehľadnosť pri prechode myši';
+$lang['imgp_preloadImages'] = 'predhrať fotku (neodporúčané pri pomalom hostingu)';
+$lang['imgp_send'] = 'Poslať!';
+$lang['imgp_showtitle'] = 'Zobraziť meno fotky';
+?>
