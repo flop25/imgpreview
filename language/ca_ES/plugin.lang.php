6119cfc556302fe23ee69ba93ce3f0e4e622257e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-09 16:24:22 +0000

    [image_preview] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@14882 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_conf_updated'] = 'La configuració s\'ha actualitzat';
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> - El desenvolupador d\'aquest complement recomana el següent:
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> Desenvolupat per P@t : Aquest complement afegeix mòduls a la galeria.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> desenvolupat per Flop25 : Aquest complement permet utilitzar icones d\'altres temes</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> desenvolupat per Flop25 : flop_style és una "extensió de plantilla" i permetre canvis d\'una petita part de Piwigo com ara la visualització d\'àlbums</li>
+<li><a href="http://piwigo.org/ext/">i molt més a piwigo.org</a></li>
+</ul>
+<br>
+Per als dissenyadors: la finestra emergent es pot personalitzar amb l\'identificador #tooltip (amb img o span per al text)
+<br>
+<br>
+Aquest complement utilitza una versió modificada de:<br>
+ 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>';
+$lang['imgp_max_height'] = 'Alçada màxima de la imatge';
+$lang['imgp_max_width'] = 'Amplada màxima de la imatge';
+$lang['imgp_opacity'] = 'Opacitat al passar el ratolí per sobre';
+$lang['imgp_preloadImages'] = 'Precarregar les imatges (poc recomanable en allotjaments amb poca velocitat)';
+$lang['imgp_send'] = 'Enviat!';
+$lang['imgp_showtitle'] = 'Mostrar el nom de la imatge';
+?>
