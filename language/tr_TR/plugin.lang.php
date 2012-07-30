6c0609adfc5991669c44fce7edef725cf0b5e95a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-30 19:14:20 +0000

    [image_preview] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@17228 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -1,34 +1,33 @@
 <?php
-$lang['imgp_max_width'] = 'Resmin maksimum genişliği';
-$lang['imgp_max_height'] = 'Resmin maksimum yüksekliği';
-$lang['imgp_send'] = 'Gönder!';
+$lang['imgp_max_width'] = 'azami resim genişliği';
+$lang['imgp_max_height'] = 'azami resim yüksekliği';
+$lang['imgp_send'] = 'Gönder !';
 
 $lang['imgp_conf_updated'] = 'Yapılandırma güncellendi';
 
 
-$lang['imgp_showtitle'] = 'Resim ismini görüntüle';
+$lang['imgp_showtitle'] = 'Resim adını görüntüle';
 $lang['imgp_preloadImages'] = 'Resimleri önceden yükle (yavaş sunucularda önerilmez)';
-$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> -bu eklentinin yaratıcısı bunları tavsiye eder:
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> -bu eklentinin yaratıcısı- tavsiye eder :
 <ul>
-<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t : this plugin adds modules on your gallery.</li>
-<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25 : this plugin allows you to use other icons for any themes</li>
-<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> by Flop25 : flop_style is a "template extension" and enable changes of a small part of Piwigo such as categories display</li>
-<li><a href="http://piwigo.org/ext/">and much more on piwigo.org</a></li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t : bu eklenti galerinize modüller ekler.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25 : bu eklenti herhangi bir temanın diğer ikonlarını kullanmanıza olanak tanır.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> by Flop25 : flop_style bir "şablon uzantısıdır" ve kategori ekranları gibi küçük bölümlerin değiştirilmesini etkinleştirir.</li>
+<li><a href="http://piwigo.org/ext/">ve daha fazlası için piwigo.org</a></li>
 </ul>
 <br>
-Dizayncılara: açılır pencereler #tooltip (yazı için img veya span ile) etiketi ile kişiselleştirilebilir.
+Geliştiriciler için : açılır pencere #tooltip kimliği ile kişiselleştirilebilir (yazı için img veya span ile)
 <br>
 <br>
-This plugin uses a modified version of :<br>
+Bu eklenti aşağıdaki eklentinin değiştirilmiş bir versiyonudur:<br>
  
- * imgPreview jQuery plugin<br>
- * Copyright (c) 2009 James Padolsey<br>
+ * imgPreview jQuery eklentisi<br>
+ * Kopya hakları (c) 2009 James Padolsey<br>
  * j@qd9.co.uk | http://james.padolsey.com<br>
- * Dual licensed under MIT and GPL.<br>
- * Updated: 09/02/09<br>
- * @author James Padolsey<br>
- * @version 0.22<br>
-';
+ * MIT ve GPL altında çifte lisans.<br>
+ * Güncelleme: 09/02/09<br>
+ * @yazar James Padolsey<br>
+ * @sürüm 0.22<br>';
 <ul>
 <li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t : this plugin adds modules on your gallery.</li>
 <li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25 : this plugin allows you to use other icons for any themes</li>
