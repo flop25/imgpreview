3425705a5f8ae98d9f3088b8fd079dbf18b03529
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-21 14:52:57 +0000

    [image_preview] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@18120 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_conf_updated'] = 'Opsætningen er opdateret';
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> -forfatteren af denne plugin, anbefaler:
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> af P@t: plugin\'en tilføjer moduler til dit galleri.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> af Flop25: plugin\'en giver mulighed for at anvende andre ikoner i ethvert tema</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> af Flop25: flop_style er en "template extension" (skablonudvidelse) og gør det muligt at ændre små dele af Piwigo, så som kategorivisning</li>
+<li><a href="http://piwigo.org/ext/">og mange andre på piwigo.org</a></li>
+</ul>
+<br>
+Til designere: popup\'en kan tilpasses med id\'en #tooltip (med img eller span til teksten)
+<br>
+<br>
+Plugin\'en anvender en ændret udgave af:<br>
+ 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>';
+$lang['imgp_max_height'] = 'maksimal højde på billedet';
+$lang['imgp_max_width'] = 'maksimal bredde på billedet';
+$lang['imgp_opacity'] = 'dækkeevne ved mouseover';
+$lang['imgp_preloadImages'] = 'Forindlæs billederne (ikke anbefalet på en langsom værtsmaskine)';
+$lang['imgp_send'] = 'Gem!';
+$lang['imgp_showtitle'] = 'Vis billednavnet';
+?>
