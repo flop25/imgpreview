27396522deaf8e70a732888d15565bf8391bf9df
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-09 22:17:55 +0000

    [image_preview] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@18583 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_conf_updated'] = '设置已更新';
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> -本插件的作者- 推荐 :
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t : 该插件为您的相册增加模块</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25 : 该插件使您可以在任何主题中使用其他的图标</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> by Flop25 : flop_style 是一个"模板扩展"，可以改变Piwigo程序的一小部分，比如类别的显示方式</li>
+<li><a href="http://piwigo.org/ext/">更多插件尽在piwigo.org</a></li>
+</ul>
+<br>
+致设计人员 : 弹窗可以用 id #tooltip 进行定制 (使用针对文本的 img 或 span 标签)
+<br>
+<br>
+本插件使用了以下程序的修改版 :<br>
+ 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>';
+$lang['imgp_max_height'] = '图片最大高度';
+$lang['imgp_max_width'] = '图片最大宽度';
+$lang['imgp_opacity'] = '鼠标悬停时的透明度';
+$lang['imgp_preloadImages'] = '预加载图片（不建议速度慢的主机启用）';
+$lang['imgp_send'] = '提交！';
+$lang['imgp_showtitle'] = '显示图片名称';
+?>
