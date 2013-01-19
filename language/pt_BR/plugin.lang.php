0998717fcc50a1823784fb0ec340ced0a2038a26
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-19 21:38:35 +0000

    [image_preview] Add pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@20271 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,52 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_conf_updated'] = 'A configuração foi atualizada';
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> - o criador desse  plugin- recomenda :
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> por P@t : esse plugin adiciona módulos nos seus álbuns.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> por Flop25 : Esse plugin te permite usar outros ícones para alguns temas</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> por Flop25 : flop_style é um "template extensão" ele habilita mudanças no Piwigo como a visualização das categorias</li>
+<li><a href="http://piwigo.org/ext/">e muito mais em piwigo.org</a></li>
+</ul>
+<br>
+Para designers : o popup é personalizável com o id #tooltip (com img ou span para texto)
+<br>
+<br>
+Esse plugin usa um versão modificada de :<br>
+ 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Licenciado sob MIT e GPL.<br>
+ * Atualizado: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>
+
+		';
+$lang['imgp_max_height'] = 'Altura máxima da foto';
+$lang['imgp_max_width'] = 'Largura máxima da foto';
+$lang['imgp_opacity'] = 'Opacidade ao passar o mouse';
+$lang['imgp_preloadImages'] = 'Précarrega as fotos (não recomendado em servidores lentos)';
+$lang['imgp_send'] = 'Submeta!';
+$lang['imgp_showtitle'] = 'Mostra o nome da foto';
+?>
