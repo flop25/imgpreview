02ca35905b366b6b4bb30070e315f684b4fb659a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-04 07:30:14 +0000

    [image_preview] Add bg_BG, thanks to : picsvet
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@21168 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_conf_updated'] = 'Актуализация';
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> -създателите на този плъгин препоръчват:
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Подръжка</a> by P@t : този плъгин добавя модули в галерията ви.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Зададете икони</a> by Flop25 : този плъгин ви позволява да използвате други икони за всякакви теми</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop стил</a> by Flop25 : флоп стил е "шаблон продължение" дава възможност за промени на малка част от Piwigo като изглед на категории</li>
+<li><a href="http://piwigo.org/ext/">повече за piwigo.org</a></li>
+</ul>
+<br>
+За дизайнерите: появилото се персонализира с номер # подсказка (с изображение или диапазон на текста)<br>
+<br>
+Този плъгин използва модифицирана версия на :<br>
+ 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual лицензиран под MIT and GPL.<br>
+ * Обновена: 09/02/09<br>
+ * @автор James Padolsey<br>
+ * @версия 0.22<br>
+';
+$lang['imgp_showtitle'] = 'Показване името на снимката';
+$lang['imgp_send'] = 'Подаване!';
+$lang['imgp_preloadImages'] = 'Заредени снимки (не се препоръчва на бавен хост)';
+$lang['imgp_opacity'] = '(Непрозрачност)Задържане на курсора на мишката';
+$lang['imgp_max_width'] = 'максимална ширина на картината';
+$lang['imgp_max_height'] = 'максимална височина на снимката';
+?>
