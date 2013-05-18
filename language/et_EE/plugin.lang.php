924178b11c15c3f977543c96ddf06e6cec493709
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-18 11:14:04 +0000

    [image_preview] Add et_EE, thanks to : kristian
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@22733 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> --selle plugina looja-- soovitab veel:
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t : plugin lisab mooduleid su galeriise.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25 : plugin lubab kasutada teisi ikoone mis tahes teema juures.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> by Flop25 : flop_style võimaldab mallide kaudu teha väikseid muudatusi Piwigosse, nagu nt kategooriate näitamist.</li>
+<li><a href="http://piwigo.org/ext/">ja palju veel piwigo.org-ist</a></li>
+</ul>
+<br>
+Disaineritele : hüpikaken on kohandatav id #tooltip-iga (kasutades \'img\' või \'span\' silte teksti jaoks)
+<br>
+<br>
+Käesolev plugin kasutab muudatud koodi teistelt:<br>
+ 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>';
+$lang['imgp_conf_updated'] = 'Sätted uuendati';
+$lang['imgp_max_height'] = 'pildi suurim lubatud kõrgus';
+$lang['imgp_max_width'] = 'pildi suurim lubatud laius';
+$lang['imgp_opacity'] = 'Pildi läbipaistvus kui hiir liigub selle üle';
+$lang['imgp_preloadImages'] = 'Laadi pildid juba ette (ei ole soovitatud aeglastel arvutitel)';
+$lang['imgp_send'] = 'Postita muudatused!';
+$lang['imgp_showtitle'] = 'Kuva pildi nime';
+?>
