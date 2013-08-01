2ca4803e490370f2d07627df948529ffded59b1a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-01 19:38:40 +0000

    [image_preview] Add sv_SE, thanks to : kabo
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@24103 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_conf_updated'] = 'Konfigurationen har blivit uppdaterad';
+$lang['imgp_max_height'] = 'maxhöjd på bilden';
+$lang['imgp_max_width'] = 'maxbredd på bilden';
+$lang['imgp_opacity'] = 'Opacitet vid mouseover';
+$lang['imgp_preloadImages'] = 'Förladda bilderna (inte rekommenderat på en långsam server)';
+$lang['imgp_send'] = 'Skicka!';
+$lang['imgp_showtitle'] = 'Visa bildnamnet';
+?>
