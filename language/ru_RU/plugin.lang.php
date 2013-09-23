e3bb0d3357034aebe87c7a4e247d91405b5db38e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-23 16:11:03 +0000

    [image_preview] Update ru_RU, thanks to : Konve
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@24598 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -1,11 +1,11 @@
 <?php
-$lang['imgp_max_width'] = 'максимальная ширина изображения';
-$lang['imgp_max_height'] = 'максимальная высота изображения';
+$lang['imgp_max_width'] = 'Максимальная ширина показываемого изображения';
+$lang['imgp_max_height'] = 'Максимальная высота показываемого изображения';
 $lang['imgp_send'] = 'Подтвердить!';
 
 $lang['imgp_conf_updated'] = 'Настройки сохранены';
 $lang['imgp_showtitle'] = 'Отображение названия изображения';
 $lang['imgp_preloadImages'] = 'Предварительная загрузка изображений (не рекомендуется при слабом/медленном хостинге)';
-$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> - автор этого плагина рекомендует: <ul><li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t: этот плагин добавляет блоки в меню Вашей галереи.</li> <li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25: этот плагин позволяет использовать разные иконки для любых тем.</li> <li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> by Flop25: flop_style это "расширение для шаблонов" и позволяет делать небольшие изменеия в Piwigo, как например, отображение категорий</li> <li><a href="http://piwigo.org/ext/">а также много всего полезного на piwigo.org и ru.piwigo.org</a></li> </ul> <br> Примечание для дизайнеров: the popup is customizable with the id #tooltip (with img or span for the text) <br> <br> Этот плагин использует следующие модификации:<br> * imgPreview jQuery plugin<br> * Copyright (c) 2009 James Padolsey<br> * j@qd9.co.uk | http://james.padolsey.com<br> * Dual licensed under MIT and GPL.<br> * Updated: 09/02/09<br> * @author James Padolsey<br> * @version 0.22<br>';
-$lang['imgp_opacity'] = 'Непрозрачно при наведении курсора мышки';
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> - автор этого плагина рекомендует: <ul><li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t: этот плагин добавляет блоки в меню Вашей галереи.</li> <li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25: этот плагин позволяет использовать разные иконки для любых тем.</li> <li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> by Flop25: flop_style это "расширение для шаблонов" и позволяет делать небольшие изменеия в Piwigo, как например, отображение категорий</li> <li><a href="http://piwigo.org/ext/">а также много всего полезного на piwigo.org и ru.piwigo.org</a></li> </ul> <br> Примечание для разработчиков: Всплывающее окно можно настроить с ID #tooltip (с изображением или диапазоном для текста<br> <br> Этот плагин использует следующие модификации:<br> * imgPreview jQuery plugin<br> * Copyright (c) 2009 James Padolsey<br> * j@qd9.co.uk | http://james.padolsey.com<br> * Dual licensed under MIT and GPL.<br> * Updated: 09/02/09<br> * @author James Padolsey<br> * @version 0.22<br>';
+$lang['imgp_opacity'] = '"Затенение" миниатюры при наведении курсора мышки';
 ?>
