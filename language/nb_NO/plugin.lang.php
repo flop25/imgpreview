feb345d6330bec2c435531e5808620367beeb4d6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-11 19:10:57 +0000

    [image_preview] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@30010 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> -skaperen av dette programtilleget - anbefaler:
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t : dette tillegsprogrammet legger moduler til ditt galleri.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25 : Dette programtillegget gjør det mulig å bruke andre ikoner for alle temaer</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> by Flop25 : flop_style er en "mal utvidelse" som muliggjøre endringer av en liten del av Piwigo som for eksempel kategorien skjerm</li>
+<li><a href="http://piwigo.org/ext/">og mye mer på piwigo.org</a></li>
+</ul>
+<br>
+For designere : popup er tilpasselig med id #tooltip (med img eller span for teksten)
+<br>
+<br>
+Dette programtillegget bruker en modifisert versjon av :<br>
+ 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>';
+$lang['imgp_conf_updated'] = 'Config er oppdatert';
+$lang['imgp_max_height'] = 'maksimal høyde på bildet';
+$lang['imgp_max_width'] = 'maksimal bredde på bildet';
+$lang['imgp_opacity'] = 'Gjennomsiktig ved mus over';
+$lang['imgp_preloadImages'] = 'Forhåndslasting av bilder (ikke anbefale med treg vert)';
+$lang['imgp_send'] = 'Send inn!';
+$lang['imgp_showtitle'] = 'Vis bildets navn';
