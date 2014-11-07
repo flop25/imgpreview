1e44ee3ffea58dba9e7bd39facec26d212d09b87
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-07 11:48:44 +0000

    [image_preview] Update fi_FI, thanks to lrd
    git-svn-id: http://piwigo.org/svn/extensions/imgpreview@30381 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['imgp_howitworks'] = '<a href="http://www.planete-flop.fr">Flop25</a> -tämän laajennuksen luoja, suosittelee :
+<ul>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=190">PWG Stuff</a> by P@t : Tämä laajennus lisää moduuleita galleriaasi.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=527">Icons Set</a> by Flop25 : Tämä laajennus mahdollistaa kaikkien kuvakkeiden käytön kaikissa teemoissa.</li>
+<li><a href="http://piwigo.org/ext/extension_view.php?eid=231">Flop Style</a> by Flop25 : flop_style on templaattilaajennus ja mahdollistaa muutoksia pieniin osaan Piwigoa, kuten kategorioiden ulkoasuun</li>
+<li><a href="http://piwigo.org/ext/">ja paljon muuta osoitteessa piwigo.org</a></li>
+</ul>
+<br>
+Ulkoasua muokkaaville : ponnahdusikkuna on kustomoitavissa HTML elementti tunnisteen (id) #tooltip avulla (img-tagi kuvalle tai span-tagi tekstille)<br>
+<br>
+Tämä laajennus käyttää muokattuja versioita seuraavista :<br>
+ 
+ * imgPreview jQuery plugin<br>
+ * Copyright (c) 2009 James Padolsey<br>
+ * j@qd9.co.uk | http://james.padolsey.com<br>
+ * Dual licensed under MIT and GPL.<br>
+ * Updated: 09/02/09<br>
+ * @author James Padolsey<br>
+ * @version 0.22<br>';
+$lang['imgp_max_height'] = 'kuvan suurin sallittu korkeus';
+$lang['imgp_max_width'] = 'kuvan suurin sallittu leveys';
+$lang['imgp_opacity'] = 'Läpinäkyvyys hiiren osoittimen ollessa kuvan päällä';
+$lang['imgp_preloadImages'] = 'Esilataa kuvat (ei suositella hitaassa ympäristössä)';
+$lang['imgp_send'] = 'Lähetä';
+$lang['imgp_showtitle'] = 'Näytä kuvan nimi';
